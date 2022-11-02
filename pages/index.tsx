62a24967c4e185c4dabceed594fd26cc875796f7
Noman Ejaz <nomanejaz132@gmail.com>
2022-11-02 21:46:33 +0500

    landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image';
-import CodeSnippet from '../public/code-snippet.png';
+import Image from "next/image";
+import CodeSnippet from "../public/code-snippet.png";
 
 export default function Home() {
   return (
     <div className="border-x border-line-1 bg-primary-2 h-screen overflow-hidden">
-      <div className="lg:px-12 xl:px-24 2xl:px-32">
+      <div className="container mx-auto lg:px-12 xl:px-24 2xl:px-32">
         <div className="flex justify-between items-center">
           <div>
             <span className="text-secondary-5 text-lg font-firacode font-normal leading-6">
@@ -14,7 +14,7 @@ export default function Home() {
               Noman Ejaz
             </h1>
             <p className="text-gradients-1 text-3xl font-firacode font-medium leading-normal">
-              {'>'} Frontend Engineer
+              {">"} Frontend Engineer
             </p>
           </div>
           <div
