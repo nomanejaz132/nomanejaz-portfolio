4f17c89ef14c2a3c5e92557f74ef38bc7e43f4a0
Noman Ejaz <nomanejaz132@gmail.com>
2023-02-07 22:07:41 +0500

    hover effect

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   EmailIcon,
   HobbiesIcon,
@@ -9,6 +9,7 @@ import {
 import SibebarDropdown from "./SibebarDropdown";
 
 const Sidebar = () => {
+  const [tab, setTab] = useState(false);
   return (
     <div className="flex">
       <div className="h-[calc(100vh-128px)] py-3 px-4 border-r border-[#1E2D3D]">
@@ -31,17 +32,20 @@ const Sidebar = () => {
         <SibebarDropdown title="contacts">
           <div className="flex flex-col gap-3">
             <a
-              className="flex items-center gap-2"
+              className="group flex items-center gap-2"
               href="mailto:hi@nomanejaz.dev"
             >
               <EmailIcon />
-              <p className="text-[#607B96] text-base font-firacode leading-5">
+              <p className="text-[#607B96] text-base font-firacode leading-5 group-hover:text-white transition-colors duration-400 ease-in-out">
                 hi@nomanejaz.dev
               </p>
             </a>
-            <a className="flex items-center gap-2" href="tel:+923049705035">
+            <a
+              className="group flex items-center gap-2"
+              href="tel:+923049705035"
+            >
               <PhoneIcon />
-              <p className="text-[#607B96] text-base font-firacode leading-5">
+              <p className="text-[#607B96] text-base font-firacode leading-5 group-hover:text-white transition-colors duration-400 ease-in-out">
                 +923049705035
               </p>
             </a>
