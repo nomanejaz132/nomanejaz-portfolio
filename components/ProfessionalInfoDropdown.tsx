1781b9b7f158c1a95df258fe0d98edd6ce993db9
Noman Ejaz <nomanejaz132@gmail.com>
2023-02-21 23:58:11 +0500

    title changes

--- components/ProfessionalInfoDropdown.tsx
+++ components/ProfessionalInfoDropdown.tsx
@@ -7,7 +7,7 @@ const ProfessionalInfoDropdown = () => {
   return (
     <SibebarDropdown title="perfessional-info">
       <div className="flex flex-col">
-        <SubDropdown title="bio" color="#E99287">
+        <SubDropdown title="pro" color="#E99287">
           <div className="flex items-center gap-2.5 pl-9 p-1.5 cursor-pointer select-none hover:bg-[#607B964D]">
             <FileIcon />
             <p className="text-[#607B96] text-base font-firacode leading-5 group-hover:text-white transition-colors duration-400 ease-in-out">
